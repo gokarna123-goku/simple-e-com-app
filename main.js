e8aec413d4ce58a45572830ca3554275c3eca736
gokarna123-goku <agokarna444@gmail.com>
2023-06-04 11:32:51 +0545

    added fix navbar while scroll y by 30px

---
+++ main.js
@@ -0,0 +1,14 @@
+//
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector("nav");
+    var scrollY = window.scrollY;
+
+    if (scrollY >= 30) {
+        navbar.classList.add("fixed");
+        navbar.style.transition = "0.5s ease-out";
+    } else {
+        navbar.classList.remove("fixed");
+        navbar.style.transition = "0.5s ease-out";
+    }
+});
+  
