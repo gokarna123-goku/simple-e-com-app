02efb4fa01157e67a29aaa43e1ad7a63a1fcdfa1
gokarna123-goku <agokarna444@gmail.com>
2023-06-12 10:38:39 +0545

    working on the increment section

--- main.js
+++ main.js
@@ -12,3 +12,29 @@ window.addEventListener("scroll", function () {
     }
 });
   
+
+// Get references to the elements
+const decrementBtn = document.getElementById("btnDecrement");
+const incrementBtn = document.getElementById("btnIncrement");
+const counterInput = document.getElementById("counter");
+
+// Set initial counter value
+let counter = 0;
+counterInput.value = counter;
+
+// Decrement button click event
+decrementBtn.addEventListener("click", () => {
+    if (counter > 0) {
+        counter--;
+        counterInput.value = counter;
+    }
+});
+
+// Increment button click event
+incrementBtn.addEventListener("click", () => {
+    counter++;
+    counterInput.value = counter;
+});
+
+
+// END
